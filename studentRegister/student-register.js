6f678a7aaa3afb024b3087d223934d18ce7c12bc
jimm433 <jim43621203@gmail.com>
2025-03-10 04:46:03 +0800

    0310

--- studentRegister/student-register.js
+++ studentRegister/student-register.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function() {
         };
 
         // 發送註冊請求到 API 端點
-        fetch('/.netlify/functions/api/register', {
+        fetch('/.netlify/functions/api', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(registerData)
